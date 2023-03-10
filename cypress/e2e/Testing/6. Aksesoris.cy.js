ba5e3fd9e6dddb72477402f7c929ea03cf6c7078
farellaldik <farellaldi29@gmail.com>
2023-03-10 17:02:34 +0700

    Update Aksesoris

--- cypress/e2e/Testing/6. Aksesoris.cy.js
+++ cypress/e2e/Testing/6. Aksesoris.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-context('Testing Test Drive Digiroom Auto2000', () => {
+context('Testing Menu Aksesoris Digiroom Auto2000', () => {
     beforeEach('',() => {
         cy.viewport(1280,1080)
         cy.visit('https://uat.auto2000.co.id/')
@@ -43,36 +43,103 @@ context('Testing Test Drive Digiroom Auto2000', () => {
     //     cy.get('.accessories-listing-container').contains('Cargo Net')
     // })
 
-    it('Testing Pencarian Filter Mobil Baru', () => {
+    // it('Testing Pencarian Filter Aksesoris', () => {
         
+        // //Membuka Menu Aksesoris
+        // cy.get(':nth-child(4) > .categoryContainer').click()
+        // cy.wait(800)
+        // cy.url().should('eq', 'https://uat.auto2000.co.id/c/aksesoris')
+        // cy.wait(500)
+
+    //     //FIlter Model Kendaraan
+    //     cy.get(':nth-child(2) > .filter-head').click()
+    //     cy.get(':nth-child(2) > .facet__values > :nth-child(1) > .items-container > .g-row > .custom-checkbox > .checkmark').click()
+
+    //     //Filter Kategori
+    //     cy.get(':nth-child(4) > .filter-head').click()
+    //     cy.get(':nth-child(4) > .facet__values > .filter-item-col > .items-container > .g-row > .custom-checkbox > .checkmark').click()
+
+    //     //Filter Kisaran Harga
+    //     cy.get(':nth-child(5) > .filter-head').click()
+    //     cy.get('.input-price-range-to').clear()
+    //     cy.get('.input-price-range-to').type('1000000').type('{enter}')
+
+    //     //Filter Tipe Aksesoris
+    //     cy.get(':nth-child(7) > .filter-head').click()
+    //     cy.get(':nth-child(7) > .facet__values > :nth-child(1) > .items-container > .g-row > .custom-checkbox > .checkmark').click()
+
+    //     //Filter Tipe Aksesoris
+    //     cy.get(':nth-child(9) > .filter-head').click()
+    //     cy.get(':nth-child(9) > .facet__values > :nth-child(1) > .items-container > .g-row > .custom-checkbox > .checkmark').click()
+
+    //     cy.wait(500)
+    // })
+
+    it('Testing Pembelian Aksesoris', () => {
+
         //Membuka Menu Aksesoris
-        cy.get(':nth-child(4) > .categoryContainer').click()
         cy.wait(800)
+        cy.get(':nth-child(4) > .categoryContainer').click()
         cy.url().should('eq', 'https://uat.auto2000.co.id/c/aksesoris')
         cy.wait(500)
 
-        //FIlter Model Kendaraan
-        cy.get(':nth-child(2) > .filter-head').click()
-        cy.get(':nth-child(2) > .facet__values > :nth-child(1) > .items-container > .g-row > .custom-checkbox > .checkmark').click()
-
-        //Filter Kategori
-        cy.get(':nth-child(4) > .filter-head').click()
-        cy.get(':nth-child(4) > .facet__values > .filter-item-col > .items-container > .g-row > .custom-checkbox > .checkmark').click()
-
-        //Filter Kisaran Harga
-        cy.get(':nth-child(5) > .filter-head').click()
-        cy.get('.input-price-range-to').clear()
-        cy.get('.input-price-range-to').type('1000000').type('{enter}')
-
-        //Filter Tipe Aksesoris
-        cy.get(':nth-child(7) > .filter-head').click()
-        cy.get(':nth-child(7) > .facet__values > :nth-child(1) > .items-container > .g-row > .custom-checkbox > .checkmark').click()
-
-        //Filter Tipe Aksesoris
-        cy.get(':nth-child(9) > .filter-head').click()
-        cy.get(':nth-child(9) > .facet__values > :nth-child(1) > .items-container > .g-row > .custom-checkbox > .checkmark').click()
-
+        //Klik Item Aksesoris (Car Cover Exclusive S)
+        cy.get(':nth-child(28) > .accessories-tile').trigger('mouseover').contains('LIHAT DETAIL').click({force:true})
         cy.wait(500)
+        cy.contains('Highlights')
+        cy.contains('Harga')
+        cy.contains('Info Produk')
+
+        //Klik Beli Sekarang
+        cy.get('.btn-primary-white > .add-to-cart-pdp').click({force:true})
+        cy.wait(500)
+
+        //Login
+        cy.get('#login-email').type('farellaldi29@gmail.com')
+        cy.get('#login-password').type('mrsilva123')
+        cy.get('#btn-login').click()
+        cy.wait(500)
+
+        //Lanjutkan ke Pembayaran 
+        cy.wait(500)
+        cy.get('#checkoutAccessories > .btn-primary-white').click({force:true})
+
+        //Isi Data Diri (Alamat)
+        cy.get('#new-address').click()
+        cy.get('.modal-content > #createEdit-modal > #lang-form > :nth-child(2) > .g-col-12 > .form-field-section > #addressName').type('Farell Aldi Kusuma')
+        cy.get('.modal-content > #createEdit-modal > #lang-form > :nth-child(3) > .g-col-12 > .form-field-section > #addressLabel').type('Rumah')
+
+        cy.get('#addressProvince-button').click({force:true})
+        cy.wait(200)
+        cy.get('#addressProvince-menu').contains('DKI Jakarta') .click({force:true})
+        cy.wait(200)
+
+        cy.get('#addressCity-button').click({force:true})
+        cy.wait(200)
+        cy.get('#addressCity-menu').contains('Jakarta Timur') .click({force:true})
+        cy.wait(200)
+
+        cy.get('#addressDistrict-button').click({force:true})
+        cy.wait(200)
+        cy.get('#addressDistrict-menu').contains('Duren Sawit') .click({force:true})
+        cy.wait(200)
+
+        cy.get('#subDistrict-button').click({force:true})
+        cy.wait(200)
+        cy.get('#subDistrict-menu').contains('Pondok Bambu') .click({force:true})
+        cy.wait(200)
+
+        cy.get('.modal-content > #createEdit-modal > #lang-form > :nth-child(6) > :nth-child(1) > .form-field-section > #addressLine1').type('Apartemen Casablanca East Residences CA-CD 17-01, Jalan Pahlawan Revolusi No. 2, Pondok Bambu, Duren Sawit, Jakarta Timur, Jakarta')
+        cy.wait(200)
+        
+        cy.get('.modal-content > #createEdit-modal > #lang-form > :nth-child(6) > :nth-child(2) > .form-field-section > #postalCode').type('13430')
+        cy.wait(200)
+
+        cy.get('.modal-content > #createEdit-modal > #lang-form > :nth-child(7) > :nth-child(1) > .form-field-section > #addressPhone').type('82165123593')
+        cy.wait(200)
+
+        cy.get('.modal-content > #createEdit-modal > #lang-form > :nth-child(7) > :nth-child(4) > div > #addressCust').click()
+
     })
 
     // it('Pemesanan Test Drive', () => {
