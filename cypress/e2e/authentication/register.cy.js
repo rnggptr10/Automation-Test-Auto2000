1899c7380cdc56e6a1606ec4df2fb58d76aba712
rnggptr10 <ranggaputra103@gmail.com>
2023-03-13 19:40:25 +0700

    update fitur register

--- cypress/e2e/authentication/register.cy.js
+++ cypress/e2e/authentication/register.cy.js
@@ -5,7 +5,7 @@ context('Fitur Login', () => {
         // For Set Size Desktop
         cy.viewport(1280,720)
         // For Visit Web 
-        cy.visit('https://uat.auto2000.co.id/')
+        cy.visit('https://uat.auto2000.co.id/register')
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from
             // failing the test
@@ -24,21 +24,85 @@ context('Fitur Login', () => {
         },
         // Correct Values
         {
-            "nama": "",
-            "nomor_telepon": "",
-            "email": "",
-            "password": "",
-            "confirm_password": "",
+            "nama": "Fasyess",
+            "nomor_telepon": "87720490872",
+            "email": "nurinafasya@gmail.com",
+            "password": "hahahihi123",
+            "confirm_password": "hahahihi123",
         },
         // Wrong Values
     ]
 
-
-
-
-    it('Action Register', () =>{
-        cy.get('.header-nav-container > :nth-child(2) > :nth-child(2)').click()
-        cy.wait(400)
-        cy.get('.create-acc').click()
+    it('Action Register With Null Value', () =>{
+        cy.get('.ca-submit > .btn-primary-white').should('be.disabled')
+        cy.wait(4000)
     })
+
+    it('Action Register With Correct Value', () =>{
+        // Input
+        cy.get('#create-accnt-your-name').type("Fasyess")
+        cy.get('#phoneNumber').type("87720490872")
+        cy.get('#email').type("nurinafasya@gmail.com")
+        cy.get('#cr-password').type("hahahihi123")
+        cy.get('#cr-confirm-password').type("hahahihi123")
+        
+        // Checkbox
+        cy.get('.terms-container > .custom-checkbox > .checkmark').click()
+        cy.get('.modal-content > .terms-box-container > .terms-action-container > #tcclose').click()
+
+        // Button Create Account
+        cy.get('.ca-submit > .btn-primary-white').click()
+        cy.wait(4000)
+    })
+
+    it('Action Register With Wrong Value (Null Name)', () =>{
+        // Input
+        cy.get('#phoneNumber').type("87720490872")
+        cy.get('#email').type("nurinafasya@gmail.com")
+        cy.get('#cr-password').type("hahahihi123")
+        cy.get('#cr-confirm-password').type("hahahihi123")
+        
+        // Checkbox
+        cy.get('.terms-container > .custom-checkbox > .checkmark').click()
+        cy.get('.modal-content > .terms-box-container > .terms-action-container > #tcclose').click()
+
+        // Button Create Account
+        cy.get('.ca-submit > .btn-primary-white').should('be.visible')
+        cy.wait(4000)
+    })
+
+    // Phone Number is Not Required
+    it('Action Register With Wrong Value (Null Phone Number)', () =>{
+        // Input
+        cy.get('#create-accnt-your-name').type("Fasyess")
+        cy.get('#email').type("nurinafasya@gmail.com")
+        cy.get('#cr-password').type("hahahihi123")
+        cy.get('#cr-confirm-password').type("hahahihi123")
+        
+        // Checkbox
+        cy.get('.terms-container > .custom-checkbox > .checkmark').click()
+        cy.get('.modal-content > .terms-box-container > .terms-action-container > #tcclose').click()
+
+        // Button Create Account
+        cy.get('.ca-submit > .btn-primary-white').click()
+        cy.wait(4000)
+    })
+
+    // Email is Required
+    it('Action Register With Wrong Value (Null Email)', () =>{
+        // Input
+        cy.get('#create-accnt-your-name').type("Fasyess")
+        cy.get('#phoneNumber').type("87720490872")
+        cy.get('#cr-password').type("hahahihi123")
+        cy.get('#cr-confirm-password').type("hahahihi123")
+        
+        // Checkbox
+        cy.get('.terms-container > .custom-checkbox > .checkmark').click()
+        cy.get('.modal-content > .terms-box-container > .terms-action-container > #tcclose').click()
+
+        // Button Create Account
+        cy.get('.ca-submit > .btn-primary-white').should('be.disabled')
+        cy.wait(4000)
+    })
+
 })   
