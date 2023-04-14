4d21558b1ba378958737b6960d09f5521e1c418b
rnggptr10 <ranggaputra103@gmail.com>
2023-04-14 11:45:59 +0700

    update booking body paint

--- cypress/e2e/body-paint/booking.cy.js
+++ cypress/e2e/body-paint/booking.cy.js
@@ -3,8 +3,18 @@
 context('Booking Vehicle', () => {
     // data akun user
     const userData = {
-        "email" : "nurinafasya@gmail.com",
-        "password" : "hahahihi123"
+        "email" : "ranggaputra103@gmail.com",
+        "password" : "sayangkamu8"
+    }
+    const layananServiceData = {
+        "jenis_layanan" : "Kepemilikan Asuransi"  // Kepemilikan Asuransi atau SPK Tersedia
+    }
+    const bookWaktuCabangData = {
+        "provinsi_cabang" : "Jawa Barat",
+        "kota_cabang" : "Bandung",
+        "nama_cabang" : "Auto2000 Cibiru B&P",
+        "tanggal_booking" : ":nth-child(4) > :nth-child(4) > .ui-state-default",
+        "jam_booking" : "08:30"
     }
 
 
@@ -20,15 +30,15 @@ context('Booking Vehicle', () => {
             // failing the test
             return false
         })
-    })
 
-    it('Do Booking Body Paint', () =>{
         // Login
         cy.get('#login-email').type(userData.email)
         cy.get('#login-password').type(userData.password)
         cy.get('#btn-login').click()
         cy.wait(4000)
+    })
 
+    it('Do Booking Body Paint', () =>{
         // To Page Body-Paint
         cy.get(':nth-child(13) > .categoryContainer').click()
         cy.wait(4000)
@@ -36,53 +46,49 @@ context('Booking Vehicle', () => {
         // Click Button 'Pilih Kendaraan' at Page Body-paint
         cy.get('.wrapper-button-add > .btn-primary-white').click()
         cy.wait(4000)
-        
-        // Chose Car to Body-Paint
-        cy.get('.g-col > .btn-primary-white').click()
-        // cy.get(':nth-child(3) > .wrapper-add-vehicle-list-to > .g-col > .btn-primary-white').click()
-        cy.wait(4000)
+
+        // Choose Car
+        cy.get('.service-booking-tile').then($service_booking_tile =>{
+            if($service_booking_tile.find('.g-col > .btn-primary-white').length){
+                cy.get('.g-col > .btn-primary-white').click()
+            }else{
+
+            }
+        })
 
         // Click Button 'Berikutnya'
         cy.get('#ui-id-4 > .ca-submit > .btn-primary-white').click()
         cy.get('#ui-id-6 > .ca-submit > .btn-primary-white').click()
-        cy.get(':nth-child(1) > .g-col-12 > .custom-checkbox > .checkmark').click()
-        cy.wait(4000)
 
-        // Click Dropdown 'Perusahaan Asuransi' 
+        // Pilih Layanan Service
+        cy.get(':nth-child(1) > .g-col-12 > .custom-checkbox > .checkmark').click()
         cy.get('#body-paint-insurance-button').click()
         cy.get('#body-paint-insurance-menu').contains('ASURANSI ASTRA BUANA').click()
-        cy.wait(4000)
-
-        // Checkbox 'SPK Tersedia'
         cy.get(':nth-child(3) > .g-col-12 > .custom-checkbox > .checkmark').click()
-        cy.wait(4000)
-
-        // Type Field 'Jenis Service'
         cy.get('#service-required').type('body depan penyok')
-        cy.wait(4000)
-
+        
         // Click Button 'Berikutnya'
         cy.get('.body-paint-step1 > .btn-primary-white').click()
         cy.wait(4000)
 
-        // Click Dropdown 'Pilih Waktu/Cabang'
+        // Pilih Waktu/Cabang
+        // Provinsi
+        cy.get('#provinceCode-button').click()
+        cy.get('#provinceCode-menu').contains(bookWaktuCabangData.provinsi_cabang).click()
         // Kota
         cy.get('#cityCode-button').click()
-        cy.get('#cityCode-menu').contains('Jakarta Selatan').click()
-        cy.wait(4000)
-        // Cabang
+        cy.get('#cityCode-menu').contains(bookWaktuCabangData.kota_cabang).click()
+        // Nama Cabang
         cy.get('#branchCode-button').click()
-        cy.get('#branchCode-menu').contains('Auto2000 Ciledug B&P').click()
-        cy.wait(4000)
+        cy.get('#branchCode-menu').contains(bookWaktuCabangData.nama_cabang).click()
         // Tanggal
-        cy.get(':nth-child(4) > :nth-child(6) > .ui-state-default').click()
+        cy.get(bookWaktuCabangData.tanggal_booking).click()
         cy.wait(4000)
-        // Waktu
+        // Jam Booking
         cy.get('#preferredSlot-button').click()
-        cy.get('#preferredSlot-menu').contains('09:00').click()
-        cy.wait(4000)
+        cy.get('#preferredSlot-menu').contains(bookWaktuCabangData.jam_booking).click()
         
-        // Checkbox Term & Condition
+        // Term & Condition
         cy.get('.terms-container > .custom-checkbox > .checkmark').click()
         cy.get('.modal-content > .terms-box-container > .terms-action-container > #tcclose').click()
         cy.wait(4000)
