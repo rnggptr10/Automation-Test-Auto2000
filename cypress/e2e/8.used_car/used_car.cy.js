c4a2db3d57178cfba06ae50393a2f2120db853c4
rnggptr10 <ranggaputra103@gmail.com>
2023-04-27 14:21:45 +0700

    update AT used car

--- cypress/e2e/8.used_car/used_car.cy.js
+++ cypress/e2e/8.used_car/used_car.cy.js
@@ -1,157 +1,123 @@
 /// <reference types="cypress" />
 
-context('Testing Mobil Bekas (Used Car) Digiroom Auto2000', () => {
+context('Used Car', () => {
+    // data User
+    const userData = {
+        "email" : "ranggaputra103@gmail.com",
+        "password" : "sayangkamu8"
+    } 
+    
     beforeEach('',() => {
-        cy.viewport(1280,1080)
-        cy.visit('https://uat.auto2000.co.id/')
-        cy.wait(1000)
-        cy.get('.close > img').click({force:true})
+        // For Set Size Desktop
+        cy.viewport(1280,720)
+
+        // For Visit Web 
+        cy.visit('https://uat.auto2000.co.id/login')
+        
         Cypress.on('uncaught:exception', (err, runnable) => {
             // returning false here prevents Cypress from
             // failing the test
             return false
         })
+
+        // Login
+        cy.get('#login-email').type(userData.email)
+        cy.get('#login-password').type(userData.password)
+        cy.get('#btn-login').click()
+        cy.wait(4000)
     })
     
-    // it('Testing Akses Menu Mobil Bekas Via Navbar', () => {
-    //     cy.get('#menu-bar > ul.nav-list > :nth-child(2) > a').click()
-    //     cy.wait(500)
-    //     cy.get('#menu-bar > ul.nav-list > :nth-child(2) > a').click()
-    //     cy.wait(500)
+    // it('Do Search', () =>{
+    //     // To Page Used Car
+    //     cy.get(':nth-child(6) > .categoryContainer').click()
 
-    //     cy.url().should('eq', 'https://uat.auto2000.co.id/c/certified-used-car-toyota')
-    //     cy.wait(500)
+    //     // Search
+    //     cy.get('#js-site-search-input').type('TOYOTA FORTUNER 2.5 G AT' + '{enter}')
+
+    //     // Check Result
+    //     cy.contains('TOYOTA FORTUNER 2.5 G AT')
     // })
+
+    // it('Do Search By Filter', () =>{
+    //     // To Page Used Car
+    //     cy.get(':nth-child(6) > .categoryContainer').click()
     
-    // it('Testing Akses Menu Mobil Bekas Via Kategori Pilihan', () => {
-    //     cy.get(':nth-child(6) > .categoryContainer').click()
-    //     cy.wait(500)
-
-    //     cy.url().should('eq', 'https://uat.auto2000.co.id/c/certified-used-car-toyota')
-    //     cy.wait(500)
-    // })
-
-
-    // it('Testing Pencarian Spesifik Mobil Bekas', () => {
-    //     cy.get(':nth-child(6) > .categoryContainer').click()
-    //     cy.wait(500)
-
-    //     cy.url().should('eq', 'https://uat.auto2000.co.id/c/certified-used-car-toyota')
-    //     cy.wait(500)
-
-    //     cy.get('#js-site-search-input').type('Avanza').type('{enter}')
-    //     cy.wait(500)
-
-    //     cy.contains('Avanza')
-    // })
-
-    // it('Testing Pencarian Filter Mobil Baru', () => {
-    //     cy.get(':nth-child(6) > .categoryContainer').click()
-    //     cy.wait(500)
-
-    //     cy.url().should('eq', 'https://uat.auto2000.co.id/c/certified-used-car-toyota')
-    //     cy.wait(500)
-
-    //     //Filter Model Kendaraan
+    //     // Model Kendaraan
     //     cy.get(':nth-child(2) > .filter-head').click()
-    //     cy.wait(200)
+    //     cy.get(':nth-child(2) > .facet__values > :nth-child(4) > .items-container > form > .custom-checkbox > .checkmark').click()
 
-    //     cy.get(':nth-child(2) > .facet__values > :nth-child(2) > .items-container > form > .custom-checkbox > .checkmark').click({force:true})
-    //     cy.wait(200)
-
-    //     //Filter Tahun Produksi
+    //     // Tahun Produksi
     //     cy.get(':nth-child(4) > .filter-head').click()
-    //     cy.wait(200)
-
-    //     cy.get(':nth-child(4) > .facet__values > :nth-child(2) > .items-container > form > .custom-checkbox > .checkmark').click({force:true})
-    //     cy.wait(200)
-
-    //     //Filter Kisaran Harga
-    //     cy.get(':nth-child(5) > .filter-head').click()
-    //     cy.wait(200)
-
-    //     cy.get('.input-price-range-to').clear().type('200000000').type('{enter}')
-    //     cy.wait(200)
-
-    //     //Validasi
-    //     cy.contains('AVANZA')
-
+    //     cy.get(':nth-child(4) > .facet__values > :nth-child(1) > .items-container > form > .custom-checkbox > .checkmark').click()
     // })
 
-    it('Testing Lihat Produk Mobil Bekas (Rush)', () => {
+    it('Do Buy Used Car', ()=>{
+        // To Page Used Car
         cy.get(':nth-child(6) > .categoryContainer').click()
-        cy.wait(500)
-
-        cy.url().should('eq', 'https://uat.auto2000.co.id/c/certified-used-car-toyota')
-        cy.wait(500)
-        
-        //Klik Mobil Bekas Rush 2012
+    
+        // Pilih Mobil (Rush)
         cy.get('[data-product-code="RUS028A20"]').invoke('show').contains('Lihat Produk').click({force: true});
         cy.url().should('eq', 'https://uat.auto2000.co.id/certified-used-car-toyota/p/rus028a20')
         cy.wait(500)
 
-        //Klik Tombol Beli
+        // Button Beli
         cy.get('#addToCartFormButtonRUS028A20 > .btn-primary-white').click()
-        cy.wait(1000)
+        cy.wait(400)    
 
-        //Login
-        cy.get('#login-email').type('farellaldi29@gmail.com')
-        cy.get('#login-password').type('mrsilva123')
-        cy.get('#btn-login').click()
-        cy.wait(4000)
 
-        //Klik Lanjut Pada Pilih Paket Servis
-        cy.get('[data-slick-index="1"] > :nth-child(1) > #addServicePackageProduct > .item-border').click()
-        cy.wait(500)
-
-        cy.get('#btnMoveRightTab').click();
-        cy.wait(1000)
-
-        //Klik Lanjut Pada Pilih Paket Servis
-        cy.get('.slick-current > :nth-child(1) > .product-carousel-item > .acce-item-border > .custom-checkbox > .checkmark').click()
-        cy.wait(500)
-
-        cy.get('#btnMoveRightTab').click();
-        cy.wait(1000)
-
-        //Klik Lanjut Pembayaran
-        cy.get('#btncontpayment').click();
-        cy.wait(1000)
-
-        //Klik Pembayaran Tunai Virtual Account
+        /***********************************
+         *        Paket Servis
+         **********************************/
+        // Opsi Service
+        // Tidak Membutuhkan Paket Service
+        cy.get('#noNeedServiceUsedCar > .product-carousel-text-section').click()
+        
+        // Button Lanjut
+        cy.get('#btnMoveRightTab').click()
+        
+        /***********************************
+         *           Aksesoris
+        **********************************/
+        // Opsi Aksesoris
+        cy.get('[data-slick-index="1"] > :nth-child(1) > .product-carousel-item > .acce-item-border > .custom-checkbox > .checkmark').click()
+        
+        // Button Lanjut
+        cy.get('#btnMoveRightTab').click()
+        // Button Lanjut ke Pembayaran
+        cy.get('#btncontpayment').click()
+       
+        /***********************************
+            *        Opsi Pembayaran
+        **********************************/
+        // Pembayaran Tunai
         cy.get('#btnNewCarPaymentUrl').click()
-        cy.wait(1000)
-
-        //Upload KTP
+        cy.wait(400)
+        
+        /***********************************
+         *      Informasi Pelanggan
+        **********************************/
+        // Upload KTP
         cy.get('input[type=file]').attachFile('../fixtures/img/contoh_KTP.jpg')
         cy.wait(1000)
-
         //Isi Alamat
         cy.get('#used-car-address').type('Apartemen Casablanca East Residences').type('{enter}')
         cy.wait(200)
+        // Alamat Detail
         cy.get('#used-car-flat').type('CA-CD 17-01')
         cy.wait(200)
-
         //Isi Kode Pos
         cy.get('#used-car-postal').type('13440').type('{enter}')
         cy.wait(200)
-
         //Input NPWP
-        cy.get('#used-car-npwp').type('12345678901234').type('{enter}')
+        cy.get('#used-car-npwp').type('111111111111111').type('{enter}')
+        cy.wait(200)
+        
+        // Checkbox Term & Condition
+        cy.get('.terms-container > .custom-checkbox > .checkmark').click()
+        cy.get('.modal-content > .terms-box-container > .terms-enable-scroll > .terms-action-container > #tcclose').click()
         cy.wait(200)
 
-        //Checklist Kotak
-        cy.get('.terms-container > .custom-checkbox > .checkmark').click()
-        cy.wait(500)
-
-        //Klik Close
-        cy.get('.modal-content > .terms-box-container > .terms-enable-scroll > .terms-action-container > #tcclose').click()
-        cy.wait(500)
-
-        //Klik Bayar Sekarang
+        // Button Bayar Sekarang
         cy.get('#used-car-pay-now').click()
-        cy.wait(1000)
     })
-
-
 })
