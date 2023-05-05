c49dfd48c86415c47ba8bb3cab851222e590e0c1
rnggptr10 <ranggaputra103@gmail.com>
2023-05-05 13:20:21 +0700

    update at kupon service

--- cypress/e2e/5.kupon_service/kupon_service.cy.js
+++ cypress/e2e/5.kupon_service/kupon_service.cy.js
@@ -51,42 +51,42 @@ context('Kupon Service', () => {
         cy.wait(4000)
     })
     
-    // it('Do Search', () => {
-    //     // To Page Kupon Service
-    //     cy.get(':nth-child(3) > .categoryContainer').click()
+    it('Do Search', () => {
+        // To Page Kupon Service
+        cy.get(':nth-child(3) > .categoryContainer').click()
 
-    //     // Search
-    //     cy.get('.product-list-search-bar > .search-form > #js-site-search-input').type('COUPONTESTUAT17' + '{enter}')
+        // Search
+        cy.get('.product-list-search-bar > .search-form > #js-site-search-input').type('COUPONTESTUAT17' + '{enter}')
 
-    //     // Check Result
-    //     cy.get('.accessories-tile').contains('COUPONTESTUAT17')
-    //     cy.wait(400)
-    // })  
+        // Check Result
+        cy.get('.accessories-tile').contains('COUPONTESTUAT17')
+        cy.wait(400)
+    })  
 
-    // it('Do Search By Filter', () => {
-    //     // To Page Kupon Service
-    //     cy.get(':nth-child(3) > .categoryContainer').click()
+    it('Do Search By Filter', () => {
+        // To Page Kupon Service
+        cy.get(':nth-child(3) > .categoryContainer').click()
     
-    //     // Model Mobil
-    //     cy.get('#ui-id-1-button').click()
-    //     cy.get('#ui-id-1-menu').contains('AGYA').click()
+        // Model Mobil
+        cy.get('#ui-id-1-button').click()
+        cy.get('#ui-id-1-menu').contains('AGYA').click()
 
-    //     // Tipe Mobil
-    //     cy.get('#ui-id-2-button').click()
-    //     cy.get('#ui-id-2-menu').contains('AGYA 1.0 E A/T').click()
-    // })  
+        // Tipe Mobil
+        cy.get('#ui-id-2-button').click()
+        cy.get('#ui-id-2-menu').contains('AGYA 1.0 E A/T').click()
+    })  
     
-    // it('Do See Detail Kupon Service & Back To Landing Page Kupon Service', () => {
-    //     // To Page Kupon Service
-    //     cy.get(':nth-child(3) > .categoryContainer').click()
+    it('Do See Detail Kupon Service & Back To Landing Page Kupon Service', () => {
+        // To Page Kupon Service
+        cy.get(':nth-child(3) > .categoryContainer').click()
     
-    //     // See Detail Kupon Service
-    //     cy.get(':nth-child(3) > .accessories-tile > .accs-tile-hover > .accessories-details').click()
-    //     cy.get('.arrow-back > a').click()
+        // See Detail Kupon Service
+        cy.get(':nth-child(3) > .accessories-tile > .accs-tile-hover > .accessories-details').click()
+        cy.get('.arrow-back > a').click()
 
-    //     // Banyak List Aksesoris
-    //     cy.get('.product-list-result-count').contains('29 hasil')
-    // })  
+        // Banyak List Aksesoris
+        cy.get('.product-list-result-count').contains('29 hasil')
+    })  
 
     it('Do Add Kupon Service & Checkout', () => {
         // To Page Kupon Service
